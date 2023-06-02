054a10e4b43b71bdb4fe9f9b40fddef1f57033b4
wandakim <nwd1583@naver.com>
2023-06-02 15:32:36 +0900

    add mysql & .env

---
+++ db/database.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+});
+
+export const db = pool.promise();
