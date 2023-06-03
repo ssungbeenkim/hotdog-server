f5fd3af1166a9d55d3bce1150f29bafc8b701687
wandakim <nwd1583@naver.com>
2023-06-03 18:31:20 +0900

    Prep Heroku Deployment

--- db/database.js
+++ db/database.js
@@ -3,12 +3,14 @@ import SQ from 'sequelize';
 dotenv.config();
 
 export const sequelize = new SQ.Sequelize(
-  process.env.DB_NAME,
+  process.env.DB_DATABASE,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
+
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
     logging: false,
     dialectOptions: {
       ssl: {
